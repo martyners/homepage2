33d871bfd3fb8b9aa7b190ad17d7ffd09042c53b
Martyna Michalak-Halinska <martyna.michalak@gmail.com>
2021-05-02 06:43:19 +0200

    function added to js file

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
-console.log("Łapa!");
+{
 
-let buttonChangeBackground = document.querySelector(".js-buttonChangeBackground");
-let container = document.querySelector(".js-container");
-let buttonHideImg = document.querySelector(".js-hideImg");
-let photoRajan = document.querySelector(".js-photoRajan");
+    const welcomeMessage = () => console.log("Łapa!");
+
+    const toggleBackground = () => {
+        const container = document.querySelector(".js-container");
+
+        container.classList.toggle("container-background");
+        buttonChangeBackground.innerText = container.classList.contains("container-background") ? "Zmien tło na białe" : "Zmień tło na szare";
+    };
+
+    const init = () => {
+        welcomeMessage();
+
+        const buttonChangeBackground = document.querySelector(".js-buttonChangeBackground");
+        buttonChangeBackground.addEventListener("click", toggleBackground);
+
+        const buttonHideImg = document.querySelector(".js-hideImg");
+
+        buttonHideImg.addEventListener("click", () => {
+            const photoRajan = document.querySelector(".js-photoRajan");
+            photoRajan.remove();
+        });
 
 
-buttonHideImg.addEventListener("click", () => {
-    photoRajan.remove();
-})
+    };
 
-
-buttonChangeBackground.addEventListener("click", () => {
-    container.classList.toggle("container-background");
-    buttonChangeBackground.innerText = container.classList.contains("container-background") ? "Zmien tło na białe" : "Zmień tło na szare";
-});
+    init();
+}
